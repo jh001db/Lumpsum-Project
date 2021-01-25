37fa3cc3197da214a2ddd2d052479be69433e5fc
Jeroen Hendrikx <75845625+jh001db@users.noreply.github.com>
2021-01-25 22:18:06 +0100

    Update SusPens_1.py

--- SusPens_1.py
+++ SusPens_1.py
@@ -216,7 +216,8 @@ if SB2 > 0:
         st.markdown(f"<h2 style='text-align: center;'> {'€ ' + str(SB11) + '.-'} </h2>", unsafe_allow_html=True)
     with graph_014:
         st.markdown("<h1 style='text-align: center;'>Pensioen</h1>", unsafe_allow_html=True)
-    if SB13 > 950: st.markdown(f"<h2 style='text-align: center;'>{'€ '}{SB23:.2f}</h2>", unsafe_allow_html=True)
+    if SB23 > 950:
+        st.markdown(f"<h2 style='text-align: center;'>{'€ '}{SB23:.2f}</h2>", unsafe_allow_html=True)
     else:
         pass
     with graph_015:
